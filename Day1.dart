e55ec6fafb136d82b91931c51b2ce2772ebdd28b
MohameddTamerr <127758676+MohameddTamerr@users.noreply.github.com>
2023-05-11 17:03:52 +0300

    Update Day1.dart

--- Day1.dart
+++ Day1.dart
@@ -1,27 +1,25 @@
 import 'dart:io';
 
-import 'package:day1/day1.dart' as day1;
-
 void main(List<String> arguments) {
   String Name;
   Set<int> numbersSet = {1, 2, 3, 4, 5};
   Set<String> namesSet = {'Karim', 'Ali', 'Ahmed'};
-  double heightInMeters=1.75;
-  double heightInFeet = heightInMeters * 3.28084
+  double heightInMeters = 1.75;
+  double heightInFeet = heightInMeters * 3.28084;
   bool isGreater = 5 > 3;
   print("Enter Your Name");
-  Name=stdin.readLineSync()!;
-  print("Hello, "+Name);
-  print("Entee your age");
+  Name = stdin.readLineSync()!;
+  print("Hello, " + Name);
+  print("Enter your age");
   int Age = int.parse(stdin.readLineSync()!);
-  int days = 365*Age;
-  print('You are $days days.');
+  int days = 365 * Age;
+  print('You are $days days old.');
   print('$heightInMeters meters is equal to $heightInFeet feet.');
-Map<String, int> ages = {
-  'Karim': 30,
-  'Ali': 25,
-  'Mohamed': 19,  
-};
-ages['Youssef'] = 20;
+  Map<String, int> ages = {
+    'Karim': 30,
+    'Ali': 25,
+    'Mohamed': 19,
+  };
+  ages['Youssef'] = 20;
   print('Youssef is ${ages['Youssef']} years old.');
 }
